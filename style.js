ab392eb4df0ef0a99173d6bd96ee84ddf7e4de94
Kozo Nishida <kozo.nishida@gmail.com>
2024-03-06 18:14:57 +0900

    Add files via upload

---
+++ style.js
@@ -0,0 +1,13 @@
+[
+  {"selector":"node", "css": {
+    "border-width": "1px",
+    "content": "data(id)",
+	"background-color": "data(color)",
+	"background-image": "https://upload.wikimedia.org/wikipedia/commons/b/b4/High_above_the_Cloud_the_Sun_Stays_the_Same.jpg",
+	"background-fit": "cover cover"
+  }},
+  {"selector": "edge", "css": {
+    "line-color": "gray",
+	"label": "data(label)"
+  }}
+]
