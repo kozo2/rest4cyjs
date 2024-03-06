e89a672e1ae48848e88495f0dd277918066042b7
Myxogastria0808 <r.rstudio.c@gmail.com>
2024-03-07 05:40:00 +0900

    fix CORS

--- backend/plumber.R
+++ backend/plumber.R
@@ -15,7 +15,7 @@ library(plumber)
 cors <- function(req, res) {
   res$setHeader("Access-Control-Allow-Origin", "*")
   if (req$REQUEST_METHOD == "OPTIONS") {
-    res$setHeader("Access-Control-Allow-Methods", "*")
+    res$setHeader("Access-Control-Allow-Methods", "GET")
     res$setHeader(
       "Access-Control-Allow-Headers",
       req$HTTP_ACCESS_CONTROL_REQUEST_HEADERS
