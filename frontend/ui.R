d4c65e9cfa84e54869e13f1d05edfa333b15d9c0
Myxogastria0808 <r.rstudio.c@gmail.com>
2024-03-06 22:27:21 +0900

    change something

--- frontend/ui.R
+++ frontend/ui.R
@@ -1,12 +1,10 @@
 shinyUI(fluidPage(
-    tags$head(
-        #base setting
-        tags$link(rel = "stylesheet", type = "text/css", href = "style.css"),
-        tags$link(rel = "shortcut icon", href = "favicon.ico"),
-        tags$title("rest4cyjs")
-    ),
-    div(class = "logo-field"),
-    div(
-        cyjShinyOutput('cyjShiny')
-    )
+  tags$head(
+    #base setting
+    tags$link(rel = "stylesheet", type = "text/css", href = "style.css"),
+    tags$link(rel = "shortcut icon", href = "favicon.ico"),
+    tags$title("rest4cyjs")
+  ),
+  div(class = "logo-field"),
+  div(cyjShinyOutput('cyjShiny'))
 ))
