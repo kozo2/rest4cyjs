317b0faa708b6a3922d02966f5d2b23617b45183
Myxogastria0808 <r.rstudio.c@gmail.com>
2024-03-07 03:51:29 +0900

    resolve img problem

--- backend/plumber.R
+++ backend/plumber.R
@@ -12,19 +12,33 @@ library(plumber)
 
 
 #* @filter cors
-cors <- function(res) {
+cors <- function(req, res) {
   res$setHeader("Access-Control-Allow-Origin", "*")
-  res$setHeader("Access-Control-Allow-Method", "GET")
-  res$setHeader("Access-Control-Allow-Headers", "*")
-  plumber::forward()
+  if (req$REQUEST_METHOD == "OPTIONS") {
+    res$setHeader("Access-Control-Allow-Methods", "*")
+    res$setHeader(
+      "Access-Control-Allow-Headers",
+      req$HTTP_ACCESS_CONTROL_REQUEST_HEADERS
+    )
+    res$status <- 200
+    return(list())
+  } else {
+    plumber::forward()
+  }
 }
 
 
 #* 画像を返すエンドポイント
-#* @preempt cors
 #* @serializer contentType list(type="image/png")
 #* @get /test
 function() {
   svg_file_path <- "./assets/test.png"
   readBin(svg_file_path, "raw", file.info(svg_file_path)$size)
+}
+
+#* Example of customizing graphical output
+#* @serializer png list(width = 400, height = 500)
+#* @get /
+function(){
+  plot(1:10)
 }
